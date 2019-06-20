ec0badc7feeb2253da0f22478b934688f7f4c7ad
Nils Martel <akantorabc@yahoo.de>
2019-06-20 11:16:51 +0200

    Avoid borrow Checker shortcomings

--- src/stack_machine.rs
+++ src/stack_machine.rs
@@ -27,11 +27,30 @@ impl<'a> StackMachine<'a> {
         use Code::*;
         for op in self.operations.into_iter() {
             match *op {
-                And => self.push(self.pop() && self.pop()),
-                Or => self.push(self.pop() || self.pop()),
-                Impl => self.push(!self.pop() && self.pop()),
-                Biimpl => self.push(self.pop() == self.pop()),
-                Not => self.push(!self.pop()),
+                And => {
+                    let a = self.pop();
+                    let b = self.pop();
+                    self.push(a && b);
+                }
+                Or => {
+                    let a = self.pop();
+                    let b = self.pop();
+                    self.push(a || b);
+                }
+                Impl => {
+                    let a = self.pop();
+                    let b = self.pop();
+                    self.push(!a && b);
+                }
+                Biimpl => {
+                    let a = self.pop();
+                    let b = self.pop();
+                    self.push(a == b);
+                }
+                Not => {
+                    let a = self.pop();
+                    self.push(!a);
+                }
                 Put(v) => self.push(v),
                 Load(addr) => self.push(self.values & (1 << addr) > 0),
             }
